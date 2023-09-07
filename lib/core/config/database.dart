5e88d4790c59c13c8e2ee6c94c4060b0722bddf3
Konstantin Popovskikh <remakerja@gmail.com>
2023-09-07 20:22:22 +0600

    creating initial messages

--- lib/core/config/database.dart
+++ lib/core/config/database.dart
@@ -16,6 +16,17 @@ class MyDatabase extends _$MyDatabase {
   @override
   int get schemaVersion => 1;
 
+  @override
+  MigrationStrategy get migration {
+    return MigrationStrategy(
+      beforeOpen: (details) async {
+        if (details.wasCreated) {
+          _addInitialMessages();
+        }
+      },
+    );
+  }
+
   Future<List<ChatData>> getMessages() async {
     return await select(chat).get();
   }
@@ -26,6 +37,22 @@ class MyDatabase extends _$MyDatabase {
 
   Future<void> clearMessages() async {
     await delete(chat).go();
+  }
+
+  _addInitialMessages() async {
+    const initialMessages = [
+      ChatCompanion(
+        value: Value('initial message 1'),
+        isIncoming: Value(true),
+      ),
+      ChatCompanion(
+        value: Value('initial message 2'),
+        isIncoming: Value(false),
+      ),
+    ];
+
+    final inserts = initialMessages.map((e) => into(chat).insert(e));
+    await Future.wait(inserts);
   }
 }
 
